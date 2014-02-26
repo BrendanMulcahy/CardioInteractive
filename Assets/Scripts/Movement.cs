969d92f497532f82097af72523583388239ce553
BrendanMulcahy <bmm58@case.edu>
2014-02-26 15:17:06 -0500

    Beginning of the Blood Flow Simulator

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 5;
+    private Vector3 velocity;
+    private bool hasEntered = false;
+
+	// Use this for initialization
+	void Start () {
+        velocity = new Vector3();
+        velocity.x = Random.Range(-1f, 1f);
+        velocity.y = Random.Range(-1f, 1f);
+        velocity = velocity.normalized * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        UpdateMovement();
+	}
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        //Debug.Log("Collision with " + collisionInfo.gameObject.name);
+        Vector3 collisionNormal = collisionInfo.contacts[0].normal;
+        Vector3 parallelToWall = Vector3.Cross(collisionNormal, Vector3.forward);
+        Vector3 perpComponent = collisionNormal * Vector3.Dot(velocity, collisionNormal) * -1;
+        Vector3 paraComponent = parallelToWall.normalized * Vector3.Dot(velocity, parallelToWall);
+        velocity = new Vector3(perpComponent.x + paraComponent.x, perpComponent.y + paraComponent.y, 0f);
+        velocity = velocity.normalized * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Trigger with " + other.gameObject.name);
+        if (other.gameObject == SceneManager.Instance.mainArea && !hasEntered)
+        {
+            hasEntered = true;
+        }
+        else if (hasEntered && (other.gameObject == SceneManager.Instance.Gate1 ||
+                                other.gameObject == SceneManager.Instance.Gate2 ||
+                                other.gameObject == SceneManager.Instance.Gate3))
+        {
+            velocity = new Vector3(velocity.x, velocity.y * -1, 0);
+        }
+        else if (other.gameObject == SceneManager.Instance.destroyer)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void UpdateMovement()
+    {
+        transform.position = new Vector3(transform.position.x + velocity.x * Time.deltaTime,
+                                         transform.position.y + velocity.y * Time.deltaTime,
+                                         0f);
+    }
+}
---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+    public static SceneManager Instance;
+
+    public Transform redCell;
+    public Transform blueCell;
+    public Transform spawnPointTopLeft;
+    public Transform spawnPointBottomLeft;
+    public Transform spawnPointTopRight;
+
+    public GameObject mainArea;
+    public GameObject destroyer;
+    public GameObject Gate1;
+    public GameObject Gate2;
+    public GameObject Gate3;
+
+    public int gridX = 15;
+    public int gridY = 20;
+    public int fontSize;
+    public GUISkin guiSkin;
+    
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var clone = Instantiate(redCell, spawnPointBottomLeft.position, Quaternion.LookRotation(new Vector3(1f, 0f, 0f))) as Transform;
+            clone.parent = this.transform;
+
+            clone = Instantiate(redCell, spawnPointTopLeft.position, Quaternion.LookRotation(new Vector3(1f, 0f, 0f))) as Transform;
+            clone.parent = this.transform;
+
+            clone = Instantiate(blueCell, spawnPointTopRight.position, Quaternion.LookRotation(new Vector3(1f, 0f, 0f))) as Transform;
+            clone.parent = this.transform;
+        }
+	}
+
+    void OnGUI()
+    {
+        GUI.skin = guiSkin;
+        GUILayout.BeginArea(new Rect(0f, 0f, Screen.width, Screen.height));
+        GUILayout.BeginVertical();
+        GUILayout.FlexibleSpace();
+        for (int i = 0; i < gridX; i++)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            for (int j = 0; j < gridY; j++)
+            {
+                GUILayout.Button(i + "," + j, GUILayout.Width(Screen.width / gridY), GUILayout.Height(Screen.height / gridX));
+                GUILayout.FlexibleSpace();
+            }
+            GUILayout.EndHorizontal();
+            GUILayout.FlexibleSpace();
+        }
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+    }
+}
