4660a2bfef441d4008cf0f7013f495dbd96655aa
Brendan Mulcahy <bmm58@case.edu>
2014-05-15 14:41:38 -0400

    Wrote the first person flying script, needs collision detection

---
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float normalSpeed;
+    public float fastSpeed;
+    public float rotateSpeed;
+    private float speed;
+
+    private float rotationX;
+    private float rotationY;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = normalSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Go faster when shift is held
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            speed = fastSpeed;
+        }
+
+        // Slow down when shift is released
+        if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+        {
+            speed = normalSpeed;
+        }
+
+        // If right mouse down, then rotate camera
+        if (Input.GetMouseButton(1))
+        {
+            rotationX += Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+            rotationY = Mathf.Clamp(rotationY, -90, 90);
+
+            transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+            transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
+        }   
+
+        // Move in directions
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
