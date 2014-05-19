967c1cd12cb61ab54ccd4446ed2b510ea772db45
Brendan Mulcahy <bmm58@case.edu>
2014-05-19 15:19:37 -0400

    Rewrote the camera script so that it uses rigidbody for collision detection

--- Assets/Scripts/FirstPersonCamera.cs
+++ Assets/Scripts/FirstPersonCamera.cs
@@ -10,12 +10,14 @@ public class FirstPersonCamera : MonoBehaviour
 
     private float rotationX;
     private float rotationY;
+    private Vector3 velocity;
 
 
     // Use this for initialization
     void Start()
     {
         speed = normalSpeed;
+        Screen.showCursor = false;
     }
 
     // Update is called once per frame
@@ -33,33 +35,72 @@ public class FirstPersonCamera : MonoBehaviour
             speed = normalSpeed;
         }
 
-        // If right mouse down, then rotate camera
-        if (Input.GetMouseButton(1))
-        {
-            rotationX += Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
-            rotationY += Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
-            rotationY = Mathf.Clamp(rotationY, -90, 90);
+        // Rotate camera
+        rotationX += Input.GetAxis("Mouse X") * rotateSpeed * Time.deltaTime;
+        rotationY += Input.GetAxis("Mouse Y") * rotateSpeed * Time.deltaTime;
+        rotationY = Mathf.Clamp(rotationY, -90, 90);
 
-            transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
-            transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
-        }   
+        transform.localRotation = Quaternion.AngleAxis(rotationX, Vector3.up);
+        transform.localRotation *= Quaternion.AngleAxis(rotationY, Vector3.left);
 
         // Move in directions
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            velocity = transform.forward * speed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            velocity = -transform.right * speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.back * speed * Time.deltaTime);
+            velocity = -transform.forward * speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector3.right * speed * Time.deltaTime);
+            velocity = transform.right * speed;
         }
+
+        // Stop movement
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            velocity = Vector3.zero;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            velocity = Vector3.zero;
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            velocity = Vector3.zero;
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            velocity = Vector3.zero;
+        }
+
+        // Update position based on velocity
+        rigidbody.velocity = velocity;
+
+        if (!Input.GetKey(KeyCode.W) &&
+            !Input.GetKey(KeyCode.A) &&
+            !Input.GetKey(KeyCode.S) &&
+            !Input.GetKey(KeyCode.D))
+        {
+            rigidbody.velocity = Vector3.zero;
+        }
+        rigidbody.angularVelocity = Vector3.zero;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Entered trigger!");
+        //transform.Translate(-currentTranslation);
+        //velocity = Vector3.zero;
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        Debug.Log("Exited trigger!");
     }
 }
